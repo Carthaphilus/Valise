62e5e8fb650ddf5a9622f3f31b1e51e81737b00a
FullCodex <fullcodex974@gmail.com>
2021-01-28 20:39:01 +0400

    Valise v.0.0.5

---
+++ Assets/Script/PickAbleItem.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickAbleItem : MonoBehaviour
+{
+
+    public GameObject slot;
+    public Vector3 VectorP;
+    public Vector3 VectorP2;
+    private bool picked = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player" && picked == false)
+        {
+            PickItem();
+            picked = true;
+        }
+    }
+
+    /// <summary>
+    /// Method for picking up item.
+    /// </summary>
+    private void PickItem()
+    {
+        Rigidbody Rb = gameObject.GetComponent<Rigidbody>();
+        //Vector3 VectorP = new Vector3(0.16F, -0.107F, 0.004F);
+        //Vector3 VectorP2 = new Vector3(177.457F, 275.614F, F);
+
+        // Disable rigidbody and reset velocities
+        Rb.isKinematic = true;
+        Rb.velocity = Vector3.zero;
+        Rb.angularVelocity = Vector3.zero;
+
+        // Set Slot as a parent
+        gameObject.transform.SetParent(slot.transform);
+
+        // Reset position and rotation
+        gameObject.transform.localPosition = Vector3.zero;
+        gameObject.transform.localEulerAngles = Vector3.zero;
+
+        gameObject.transform.Find("Obj1").GetComponent<MeshCollider>().enabled = false;
+        gameObject.transform.Find("Obj1").transform.localPosition = VectorP;
+        gameObject.transform.Find("Obj1").transform.localEulerAngles = VectorP2;
+    }
+}
---
+++ Assets/Script/StartGame.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    public int compteurStart;
+    public TMP_Text textCompteur;
+    public int nbPNJ;
+    private bool run = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textCompteur.text = System.Convert.ToString(compteurStart);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    IEnumerator startGame()
+    {
+        yield return new WaitForSeconds(1F);
+        compteurStart--;
+        textCompteur.text = System.Convert.ToString(compteurStart);
+
+        if (compteurStart == 0)
+        {
+            textCompteur.text = "C'est partie !";
+            yield return new WaitForSeconds(1F);
+
+            for (int x=0; x<=nbPNJ; x++)
+            {
+
+            }
+
+            textCompteur.enabled = false;
+        }
+        else
+        {
+            StartCoroutine(startGame());
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && run == false)
+        {
+            StartCoroutine(startGame());
+            run = true;
+        }
+    }
+
+
+    public float generateRandomFloat(System.Random random, int min, int max)
+    {
+        int val = random.Next(min, max);
+        float offset = (float)val;
+        return offset;
+
+
+    }
+}
