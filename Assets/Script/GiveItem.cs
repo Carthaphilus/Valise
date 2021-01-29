79b98f4f6eb1bacacb1b4ddc7b5c340fa0542cf8
FullCodex <fullcodex974@gmail.com>
2021-01-29 22:05:40 +0400

    Valise v.0.1.7

--- Assets/Script/GiveItem.cs
+++ Assets/Script/GiveItem.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GiveItem : MonoBehaviour
 {
 
     public GameObject slot;
+    public TMP_Text point;
+    private int compteur;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        compteur = 0;
     }
 
     // Update is called once per frame
@@ -25,8 +28,12 @@ public class GiveItem : MonoBehaviour
         {
             if (colliderPNJ.gameObject.GetInstanceID() == slot.transform.GetChild(0).GetComponent<WaitPnj>().getWaitPnj())
             {
+                compteur++;
                 Debug.Log("ok");
-            }else
+                point.text = System.Convert.ToString(compteur);
+                Destroy(slot.transform.GetChild(0).gameObject);
+            }
+            else
             {
                 Debug.Log("false");
             }
