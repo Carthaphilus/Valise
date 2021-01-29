b3fa9c9fc978b9f63bd33b406f69ed0f17e14de1
FullCodex <fullcodex974@gmail.com>
2021-01-29 10:04:35 +0400

    Valise v0.0.7

--- Assets/Script/StartGame.cs
+++ Assets/Script/StartGame.cs
@@ -8,6 +8,8 @@ public class StartGame : MonoBehaviour
     public int compteurStart;
     public TMP_Text textCompteur;
     public int nbPNJ;
+    public Transform PNJhomme;
+    public Transform PNJfemme;
     private bool run = false;
 
     // Start is called before the first frame update
@@ -36,7 +38,25 @@ public class StartGame : MonoBehaviour
 
             for (int x=0; x<=nbPNJ; x++)
             {
-                //y bouge pas
+                System.Random random = new System.Random();
+                Object GeneratePnJ;
+                string namePNJ;
+                float genre = Random.Range(1, 2);
+
+                Debug.Log("P:"+x);
+
+                if (genre == 1) {
+                    GeneratePnJ = Instantiate(PNJfemme, new Vector3(Random.Range(486.21F, 524.92F), 1F, Random.Range(511.52F, 609.91F)), new Quaternion());
+                    namePNJ = "PNJ_Femme_" + x;
+                }
+                else
+                {
+                    GeneratePnJ = Instantiate(PNJhomme, new Vector3(Random.Range(486.21F, 524.92F), 1F, Random.Range(511.52F, 609.91F)), new Quaternion());
+                    namePNJ = "PNJ_Homme_" + x;
+                }
+
+                GeneratePnJ.name = namePNJ;
+
 
             }
 
@@ -56,15 +76,5 @@ public class StartGame : MonoBehaviour
             StartCoroutine(startGame());
             run = true;
         }
-    }
-
-
-    public float generateRandomFloat(System.Random random, int min, int max)
-    {
-        int val = random.Next(min, max);
-        float offset = (float)val;
-        return offset;
-
-
     }
 }
