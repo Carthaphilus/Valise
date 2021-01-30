cfc1a532b65bcef5bf98078b36941a3c839ccafc
FullCodex <fullcodex974@gmail.com>
2021-01-30 21:58:31 +0400

    Valise v.0.1.9

--- Assets/Script/StartGame.cs
+++ Assets/Script/StartGame.cs
@@ -9,6 +9,8 @@ public class StartGame : MonoBehaviour
 {
     public int compteurStart;
     public float dureeGameSeconde;
+    public int nbJoueur = 0;
+    public Transform Joueur;
     public TMP_Text textCompteur;
     public int nbPNJ;
     public int nbValise = 0;
@@ -20,6 +22,112 @@ public class StartGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        /*Dictionary<string, float> config = new Dictionary<string, float>();
+        Dictionary<int, Dictionary<string, float>> joueur = new Dictionary<int, Dictionary<string, float>>();
+        Dictionary<int, Dictionary<int, Dictionary<string, float>>> nbjoueur = new Dictionary<int, Dictionary<int, Dictionary<string, float>>>();
+
+        //Si il n'a qu'un joueur
+        config.Add("x", 0);
+        config.Add("Y", 0);
+        config.Add("W", 1);
+        config.Add("H", 1);
+        joueur.Add(0, config);
+        nbjoueur.Add(1, joueur);
+        config.Clear();
+        joueur.Clear();
+
+        //Si il y'en a deux
+        config.Add("x", 0);
+        config.Add("Y", 0);
+        config.Add("W", 0.5F);
+        config.Add("H", 1);
+        joueur.Add(0, config);
+        config.Clear();
+
+        config.Add("x", 0.5F);
+        config.Add("Y", 0);
+        config.Add("W", 0.5F);
+        config.Add("H", 1);
+        joueur.Add(1, config);
+        nbjoueur.Add(2, joueur);
+        config.Clear();
+        joueur.Clear();
+
+        //Si il y'en a trois
+        config.Add("x", 0);
+        config.Add("Y", 0);
+        config.Add("W", 0.5F);
+        config.Add("H", 0.5F);
+        joueur.Add(0, config);
+        config.Clear();
+
+        config.Add("x", 0.5F);
+        config.Add("Y", 0);
+        config.Add("W", 0.5F);
+        config.Add("H", 0.5F);
+        joueur.Add(1, config);
+        config.Clear();
+
+        config.Add("x", 0);
+        config.Add("Y", 0);
+        config.Add("W", 0.5F);
+        config.Add("H", 0.5F);
+        joueur.Add(2, config);
+        nbjoueur.Add(3, joueur);
+        config.Clear();
+        joueur.Clear();
+
+        //Si il y'en a quatre
+        config.Add("x", 0);
+        config.Add("Y", 0);
+        config.Add("W", 0.5F);
+        config.Add("H", 0.5F);
+        joueur.Add(0, config);
+        config.Clear();
+
+        config.Add("x", 0.5F);
+        config.Add("Y", 0);
+        config.Add("W", 0.5F);
+        config.Add("H", 0.5F);
+        joueur.Add(1, config);
+        config.Clear();
+
+        config.Add("x", 0);
+        config.Add("Y", 0);
+        config.Add("W", 0.5F);
+        config.Add("H", 0.5F);
+        joueur.Add(2, config);
+        config.Clear();
+
+        config.Add("x", 0.5F);
+        config.Add("Y", 0.5F);
+        config.Add("W", 0.5F);
+        config.Add("H", 0.5F);
+        joueur.Add(3, config);
+        nbjoueur.Add(4, joueur);
+        config.Clear();
+        joueur.Clear();
+
+        float Jx = -1.011505F;
+        float Jz = -1.173645F;
+
+        for (int j = 0; j <= nbJoueur; j++)
+        {
+            
+            float rectX = nbjoueur[nbJoueur][j]["x"];
+            float rectY = nbjoueur[nbJoueur][j]["Y"];
+            float rectW = nbjoueur[nbJoueur][j]["W"];
+            float rectH = nbjoueur[nbJoueur][j]["H"];
+            
+
+            Transform gameJoueur;
+            Vector3 JoueurVector = new Vector3(Jx, 0.3881412F, Jz);
+            gameJoueur = Instantiate(Joueur, JoueurVector, new Quaternion());
+            gameJoueur.Find("FreeLookCameraRig").Find("Pivot").Find("Main Camera").GetComponent<Camera>().rect = new Rect(rectX, rectY, rectW, rectH);
+            Jx--;
+            Jz--;
+        }*/
+
         textCompteur.text = System.Convert.ToString(compteurStart);
     }
 
@@ -135,7 +243,8 @@ public class StartGame : MonoBehaviour
         dureeGameSeconde --;
         textCompteur.text = System.Convert.ToString(dureeGameSeconde);
         if (dureeGameSeconde <= 0) {
-            Debug.Log("Partie Finie");
+            //textCompteur.enabled = false;
+            textCompteur.text = "Partie terminé";
             Time.timeScale = 0f;
         }
         else
@@ -157,10 +266,27 @@ public class StartGame : MonoBehaviour
 
     void OnGUI()
     {
+        
         if (dureeGameSeconde <=0)
         {
-            // Si on clique sur le bouton alors isPaused devient faux donc le jeu reprend
-            if (GUI.Button(new Rect(Screen.width / 2 - 40, Screen.height / 2 - 20, 80, 40), "Restart"))
+            //Rect boxRect = new Rect(425, 100, 300, 300);
+
+            float w = 0.3F; // proportional width (0..1)
+            float h = 0.7F; // proportional height (0..1)
+            var boxRect = new Rect(0,0,0,0);
+
+            boxRect.x = (Screen.width*(1-w))/2;
+            boxRect.y = (Screen.height*(1-h))/2;
+            boxRect.width = Screen.width* w;
+            boxRect.height = Screen.height* h;
+
+            //GUIStyle style = new GUIStyle();
+            //style.fontSize = 32;
+            //GUI.Box(boxRect, "Partie Terminé");
+            //GUI.Label(new Rect(450, 400, 80, 40),GameObject.Find("Score").GetComponent<TMP_Text>().text);
+
+            // Si on clique sur le bouton alors on quitte l'application relance le temps de jeu et on recharge la scene de jeu
+            if (GUI.Button(new Rect(450, 400, 80, 40), "Rejouer"))
             {
                 Application.Quit(); // Ferme le jeu
                 Time.timeScale = 1f;
@@ -168,11 +294,11 @@ public class StartGame : MonoBehaviour
             }
             // Si on clique sur le bouton alors on ferme completment le jeu ou on charge la scene Menu Principal
             // Dans le cas du bouton Quitter, il faut augmenter sa position Y pour qu'il soit plus bas.
-            if (GUI.Button(new Rect(Screen.width / 2 - 40, Screen.height / 2 + 40, 80, 40), "Quitter"))
+            /*if (GUI.Button(new Rect(630, 400, 80, 40), "Quitter"))
             {
                 Application.Quit(); // Ferme le jeu
                 SceneManager.LoadScene("Menu Principal");
-            }
+            }*/
         }
     }
 }
